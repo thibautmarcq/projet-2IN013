082a7c686551b3ff3a6435325afd39256b95a748
thibbmrq <thibaut.marcq@gmail.com>
2024-01-23 17:50:05 +0100

    Création nouvelles classes

--- env.py
+++ env.py
@@ -1,13 +1,21 @@
 class Environnement:
     
     def __init__(self, x, y): #initialise l'environnement de taille x*y
-        self.x = x
-        self.y = y
+        self.matrice = self.init_matrice((int)(x), (int)(y)) #init de la matrice discret
+        self.rectangle = self.init_rectangle((double)(x), (double)(y)) #init du rectangle continu
         
+    def init_matrice(self, x, y):
+        return (tab[x][y])
 
+    def init_rectangle():
+        return null
+
+
+class Rectangle:
     
 
 
+class Main:
 
 
 
