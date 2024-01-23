0cd4f543a8b4cd29ea37c3c07e1dc6a0222bf591
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-01-24 00:32:54 +0100

    les derniers changements au robot, notamment un mini main permettant de tester nos methodes

--- Code/robot.py
+++ Code/robot.py
@@ -1,13 +1,17 @@
 class Obstacle :
 
-    def __init__(nom, posx, posy) :
+    def __init__(self, nom, posx, posy) :
         self.nom = nom
         self.x = posx
         self.y = posy
 
+    def presenter_obstacle(self):
+        return(self.nom + "à la position (" + str(self.x) + ", " + str(self.y) + ")")
+
 
 class Robot :
-    def __init__(self, position_x, position_y, obstacles):
+    def __init__(self, nom, position_x, position_y, obstacles):
+        self.nom = nom
         self.posx = position_x
         self.posy = position_y
         self.obstacles = obstacles
@@ -15,84 +19,94 @@ class Robot :
     def avancer(self, quantite) :
         trouve = False
         for o in self.obstacles :
-            if o.y == self.posx and (self.posy+quantite)==o.y :
+            if o.y == self.posx and (int(self.posy) + int(quantite))==o.y :
                 trouve = True
-                print("Je ne peux pas avancer en (" + self.posx + ", " + (self.posy + quantite) + ") car il y a l'obstacle " + o.nom)
+                print("Je ne peux pas avancer en (" + str(self.posx) + ", " + str(self.posy + quantite) + ") car il y a l'obstacle " + o.presenter_obstacle())
                 return
             
         if trouve == False :
-            printf("Position précédente : (" + self.posx + ", " + self.posy + "), nouvelle position : (" + self.posx + ", " + (self.posy + quantite) + ")" )
+            print("Position précédente : (" + str(self.posx) + ", " + str(self.posy) + "), nouvelle position : (" + str(self.posx) + ", " + str(self.posy + quantite) + ")" )
             self.posy = self.posy + quantite
 
 
     def reculer(self, quantite) :
-        if self.posy-quantite < 0 :
+        if (int(self.posy) - int(quantite)) < 0 :
             print("Le déplacement n'est pas possible car hors champ")
             return
         trouve = False
         for o in self.obstacles :
             if o.y == self.posx and (self.posy-quantite)==o.y :
                 trouve = True
-                print("Je ne peux pas avancer en (" + self.posx + ", " + (self.posy - quantite) + ") car il y a l'obstacle" + o.nom)
+                print("Je ne peux pas avancer en (" + str(self.posx) + ", " + str(self.posy - quantite) + ") car il y a l'obstacle" + o.presenter_obstacle())
                 return
             
         if trouve == False :
-            printf("Position précédente : (" + self.posx + ", " + self.posy + "), nouvelle position : (" + self.posx + ", " + (self.posy - quantite) + ")")
+            print("Position précédente : (" + str(self.posx) + ", " + str(self.posy) + "), nouvelle position : (" + str(self.posx) + ", " + str(int(self.posy) - int(quantite)) + ")")
             self.posy = self.posy - quantite
 
     def droite(self, quantite) :
         trouve = False
-        for o in self.osbtacles :
-            if o.x == self.posx and (self.posx + quantite) == o.x :
+        for o in self.obstacles :
+            if o.x == self.posx and (int(self.posx) + int(quantite)) == o.x :
                 trouve = True
-                print("Je ne peux pas avancer en (" + (self.posx + quantite) + ", " + self.posy + ") car il y a l'obstacle" + o.nom)
+                print("Je ne peux pas avancer en (" + str(self.posx + quantite) + ", " + str(self.posy) + ") car il y a l'obstacle" + o.presenter_obstacle())
                 return
         
         if trouve == False :
-            printf("Position précédente : (" + self.posx + ", " + self.posy + "), nouvelle position : (" + (self.posx + quantite) + ", " + self.posy + ")")
-            self.posx = self.posxy + quantite
+            print("Position précédente : (" + str(self.posx) + ", " + str(self.posy) + "), nouvelle position : (" + str(int(self.posx) + int(quantite)) + ", " + str(self.posy) + ")")
+            self.posx = int(self.posx) + int(quantite)
 
     def gauche(self, quantite) :
-        if self.posx-quantite < 0 :
+        if (int(self.posx) - int(quantite)) < 0 :
             print("Le déplacement n'est pas possible car hors champ")
             return
         trouve = False
-        for o in self.osbtacles :
-            if o.x == self.posx and (self.posx - quantite) == o.x :
+        for o in self.obstacles :
+            if o.x == self.posx and (int(self.posx) - int(quantite) )== o.x :
                 trouve = True
-                print("Je ne peux pas avancer en (" + (self.posx - quantite) + ", " + self.posy + ") car il y a l'obstacle" + o.nom)
+                print("Je ne peux pas avancer en (" + str(int(self.posx) - quantite) + ", " + str(self.posy) + ") car il y a l'obstacle" + o.presenter_obstacle())
                 return
         
         if trouve == False :
-            printf("Position précédente : (" + self.posx + ", " + self.posy + "), nouvelle position : (" + (self.posx - quantite) + ", " + self.posy + ")")
+            print("Position précédente : (" + str(self.posx) + ", " + str(self.posy) + "), nouvelle position : (" + str(self.posx - quantite) + ", " + str(self.posy) + ")")
             self.posx = self.posxy - quantite
 
-    def get_position(self) :
-        return (self.posx, self.posy)  
+    def get_position_string(self) :
+        return ("()" + str(self.posx) + ", " + str(self.posy) + ")")  
     
     def afficher_etat(self) :
-        print("Je suis le robot " + self.nom + " et je suis à la position" + self.get_position)
+        print("Je suis le robot " + self.nom + " et je suis à la position" + self.get_position_string())
 
     def deplacement(self) :
         boucle = True
         while boucle :
             print("----------------------------------")
-            print("Choisir une option : a pour avancer, r pour reculer, d pour aller a droite, g pour aller a gauche, ou q pour quitter")
+            print("Choisir une option : z pour avancer, s pour reculer, d pour aller a droite, q pour aller a gauche, ou e pour quitter")
             depl = input()
             quantite = 0
-            if depl in ['a', 'r', 'd', 'g'] :
-                print("Tapez de combien vous voulez vous déplacer")
+            if depl in ['z', 'q', 's', 'd'] :
+                print("Taper la quantité du déplacement")
                 quantite = input()
-            if depl == 'a' :
+            if depl == 'z' :
                 self.avancer(quantite)
-            elif depl == 'r' :
+            elif depl == 's' :
                 self.reculer(quantite)
             elif depl == 'd' :
                 self.droite(quantite)
-            elif depl == 'g' :
+            elif depl == 'q' :
                 self.gauche(quantite)
-            elif delp == 'q' :
+            elif depl == 'e' :
                 boucle = False
-            print("Le déplacement du robot est terminé")
+        print("Le déplacement du robot est terminé")
+        self.afficher_etat()
 
 
+o1 = Obstacle("obstacle 1", 2, 3)
+o2 = Obstacle("obstacle 2", 5, 6)
+o3 = Obstacle("obstacle 3", 1, 4)
+
+liste_obstacles = [o1, o2, o3]
+
+rob_le_robot = Robot("Robert", 1, 2, liste_obstacles)
+rob_le_robot.afficher_etat()
+rob_le_robot.deplacement()
