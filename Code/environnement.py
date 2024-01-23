b637f6ebf12902c67d6b2a46b52051d4442c9964
thibbmrq <thibaut.marcq@gmail.com>
2024-01-24 00:02:04 +0100

    Ajout et correction affichage du cadre

--- Code/environnement.py
+++ Code/environnement.py
@@ -1,25 +1,24 @@
 import numpy as np
 
-import robot as Robot
+import Robot as Robot ##
 import matrice as Matrice
 import rectangle as Rectangle
 
 class Environnement:
     
     def __init__(self, x, y): #initialise l'environnement de taille x*y
-        self.robot = Robot() #Voir avec ines les parametres
-        self.matrice = Matrice(x,y) #Instanciation de la matrice - Matrice.Matrice(x,y) ??
+        self.x=x; self.y=y
+        # self.robot = Robot.Robot() #Voir avec ines les parametres
+        self.matrice = Matrice.Matrice(x,y) #Instanciation de la matrice - Matrice.Matrice(x,y) ??
         self.rectangle = None #Constructeur du rectangle
-
-
 
 
     def trace(self):
         """Permet de tracer un environnement"""
-        for ligne in range(y):
+        for ligne in range((self.y)+1):
             print("|", end="") #Permet de ne pas sauter à la ligne à ch fois
-            for colonne in range(x):
-                if (ligne==0) or (ligne==self.matrice.y): #Premiere ou derniere ligne
+            for colonne in range(self.x):
+                if (ligne==0) or (ligne==self.y): #Premiere ou derniere ligne
                     print("-", end="") #Contour haut de la box
 
                 ## RECHERCHE DELEMENTS ET PRINT EN FCT
@@ -31,7 +30,10 @@ class Environnement:
                     print(" ", end="") #Espace (rien dans la case)
 
             print("|")
-        
+
+
+env = Environnement(25,8)
+env.trace()
 
 
 
