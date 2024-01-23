9b2721335c37437712b3a41b8014419b392e26de
thibbmrq <thibaut.marcq@gmail.com>
2024-01-23 17:39:48 +0100

    Début constructeur

--- env.py
+++ env.py
@@ -1,6 +1,11 @@
 class Environnement:
     
-    def __init__(self, )
+    def __init__(self, x, y): #initialise l'environnement de taille x*y
+        self.x = x
+        self.y = y
+        
+
+    
 
 
 
