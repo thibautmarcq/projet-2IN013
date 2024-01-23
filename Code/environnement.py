186589f086c9ce9bd85b501337448f818f7fa09f
thibbmrq <thibaut.marcq@gmail.com>
2024-01-23 23:28:38 +0100

    Mise à jour, changement de dossier

--- Code/environnement.py
+++ Code/environnement.py
@@ -7,26 +7,28 @@ import rectangle as Rectangle
 class Environnement:
     
     def __init__(self, x, y): #initialise l'environnement de taille x*y
-        self.matrice = self.init_matrice((int)(x), (int)(y)) #init de la matrice discret pour les obstacles
-        self.rectangle = self.init_rectangle((float)(x), (float)(y)) #init du rectangle continu 
-        
-    def init_matrice(self, x, y):
-        #return (tab[x][y]) #Faire qqch avec les arrays numpy
-        pass
+        self.robot = 
+        self.matrice = Matrice(x,y) #Instanciation de la matrice - Matrice.Matrice(x,y) ??
+        self.rectangle = None #Constructeur du rectangle
 
-    def init_rectangle():
-        return None
+
+
 
     def trace(self):
         """Permet de tracer un environnement"""
         for ligne in range(y):
             print("|", end="") #Permet de ne pas sauter à la ligne à ch fois
-            if (ligne==0) or (ligne==self.matrice.):
+
+            if ()
+
+            if (ligne==0) or (ligne==self.matrice.y):
                 for colonne in range(x):
                     print("-", end="") #Contour haut de la box
-                for colonne in range(x):
-                    if 
+            
+
             for colonne in range(x):
+                #recherche d'elements dans les 2 tabs (mat et rect)
+
                 print("-", end="")
             print("|")
         
