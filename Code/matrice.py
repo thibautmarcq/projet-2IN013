a7b8a4f6e7b1e284ca22ab72776f49f0d60b059a
thibbmrq <thibaut.marcq@gmail.com>
2024-01-24 18:35:57 +0100

    Correction message Matrice

--- Code/matrice.py
+++ Code/matrice.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Matrice :
     """La matrice sert à faire le lien entre la partie continue de l'environnement et la partie discrète
-    Elle prend en attribut les """
+    Elle prend en attribut les obstacle et le robot sous forme discrète"""
     
     def __init__(self, x, y):
         self.x = x
