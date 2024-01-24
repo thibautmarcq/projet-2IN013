49aa056e71d55642bb6d9d10e377d70ca6155642
thibbmrq <thibaut.marcq@gmail.com>
2024-01-24 22:53:14 +0100

    Test Tkinter ajouté

--- Code/test.py
+++ Code/test.py
@@ -1,8 +1,30 @@
 from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+# Configs de la fenêtre
+root=Tk()
+root.geometry("400x300")
+root.title("Test Tkinter")
+root.config(background="#130879")
+
+# Creation d'un label
+lab = Label(root, text="cc la tim")
+lab.place(x=50, y=10)
+
+labb = Label(root)
+labb.place_forget()
+
+# Creation d'une entrée
+inp = Entry(root)
+inp.place(x=50, y=45)
+
+# Test avec fonction et bouton
+def affiche():
+    print('coucou')
+    labb.config(text="Does this work? ->"+inp.get()+"!")
+    labb.place(x=50, y=250)
+
+btn = Button(root, text="Afficher", command=affiche)
+btn.place(x=50, y=200)
+
+# Boucle de la fenètre principale
 root.mainloop()
