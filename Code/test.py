9a8a7bf75f495b0a70dcb5ab4b9b297904c2fa5e
thibbmrq <thibaut.marcq@gmail.com>
2024-01-25 22:21:26 +0100

    Mise en commentaire de tout ce qui ne sert pas

--- Code/test.py
+++ Code/test.py
@@ -59,54 +59,54 @@ def updateVecteurL(z):
     updateVecteur(-math.pi/10)
 
 
-# Déplacement clavier
-def down(event):
-    canv.move(rect, 0, 5)
-def up(event):
-    canv.move(rect, 0, -5)
-def right(event):
-    canv.move(rect, 5, 0)
-def left(event):
-    canv.move(rect, -5, 0)
-# Déplacement boutons
-def downBtn():
-    canv.move(rect, 0, 5)
-def upBtn():
-    canv.move(rect, 0, -5)
-def rightBtn():
-    canv.move(rect, 5, 0)
-def leftBtn():
-    canv.move(rect, -5, 0)
+# # Déplacement clavier
+# def down(event):
+#     canv.move(rect, 0, 5)
+# def up(event):
+#     canv.move(rect, 0, -5)
+# def right(event):
+#     canv.move(rect, 5, 0)
+# def left(event):
+#     canv.move(rect, -5, 0)
+# # Déplacement boutons
+# def downBtn():
+#     canv.move(rect, 0, 5)
+# def upBtn():
+#     canv.move(rect, 0, -5)
+# def rightBtn():
+#     canv.move(rect, 5, 0)
+# def leftBtn():
+#     canv.move(rect, -5, 0)
 
-root.bind("<Down>", down)
-root.bind("<Up>", up)
-#root.bind("<Left>", left)
-#root.bind("<Right>", right)
+# root.bind("<Down>", down)
+# root.bind("<Up>", up)
+# #root.bind("<Left>", left)
+# #root.bind("<Right>", right)
 root.bind("<Right>", updateVecteurR)
 root.bind("<Left>", updateVecteurL)
 
-btn = Button(root, text="Up", command=upBtn)
-btn.place(x=180, y=258)
-btn = Button(root, text="Down", command=downBtn)
-btn.place(x=180, y=288)
-btn = Button(root, text="Left", command=leftBtn)
-btn.place(x=130, y=288)
-btn = Button(root, text="Right", command=rightBtn)
-btn.place(x=230, y=288)
+# btn = Button(root, text="Up", command=upBtn)
+# btn.place(x=180, y=258)
+# btn = Button(root, text="Down", command=downBtn)
+# btn.place(x=180, y=288)
+# btn = Button(root, text="Left", command=leftBtn)
+# btn.place(x=130, y=288)
+# btn = Button(root, text="Right", command=rightBtn)
+# btn.place(x=230, y=288)
 
 
-# Creation d'une entrée
-inp = Entry(root)
-inp.place(x=50, y=258)
+# # Creation d'une entrée
+# inp = Entry(root)
+# inp.place(x=50, y=258)
 
-# Test avec fonction et bouton
-def affiche():
-    print('coucou')
-    labb.config(text="Does this work? ->"+inp.get()+"!")
-    labb.place(x=50, y=288)
+# # Test avec fonction et bouton
+# def affiche():
+#     print('coucou')
+#     labb.config(text="Does this work? ->"+inp.get()+"!")
+#     labb.place(x=50, y=288)
 
-btn = Button(root, text="Change", command=affiche)
-btn.place(x=230, y=258)
+# btn = Button(root, text="Change", command=affiche)
+# btn.place(x=230, y=258)
 
 # Boucle de la fenètre principale
 root.mainloop()
