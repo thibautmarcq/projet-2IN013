609c72f799e94cf21f976bbe96390eeade16de02
thibbmrq <thibaut.marcq@gmail.com>
2024-01-25 09:19:47 +0100

    Changement titre et commentaires

--- Code/test.py
+++ Code/test.py
@@ -3,7 +3,7 @@ from tkinter import *
 # Configs de la fenêtre
 root=Tk()
 root.geometry("400x350")
-root.title("Les meilleurs Robocar Poli")
+root.title("Simulation - Robocar Poli")
 root.iconbitmap('Code\logo.ico')
 root.config(background="purple")
 
@@ -25,6 +25,7 @@ canv.create_line(0,200,300, 20) #(x1,y1, x2,y2)
 rect = canv.create_rectangle(50,x,250,150, fill='lightblue') #(xTopLeft,yTopLeft, xBtmRight,yBtmRight)
 canv.create_line(0,100,150, 20, fill='green')
 
+# Déplacement clavier
 def down(event):
     canv.move(rect, 0, 5)
 def up(event):
@@ -33,7 +34,7 @@ def right(event):
     canv.move(rect, 5, 0)
 def left(event):
     canv.move(rect, -5, 0)
-
+# Déplacement boutons
 def downBtn():
     canv.move(rect, 0, 5)
 def upBtn():
@@ -51,11 +52,11 @@ root.bind("<Right>", right)
 btn = Button(root, text="Up", command=upBtn)
 btn.place(x=180, y=258)
 btn = Button(root, text="Down", command=downBtn)
-btn.place(x=230, y=278)
+btn.place(x=180, y=288)
 btn = Button(root, text="Left", command=leftBtn)
-btn.place(x=230, y=258)
-btn = Button(root, text="Change", command=rightBtn)
-btn.place(x=230, y=258)
+btn.place(x=130, y=288)
+btn = Button(root, text="Right", command=rightBtn)
+btn.place(x=230, y=288)
 
 
 # Creation d'une entrée
