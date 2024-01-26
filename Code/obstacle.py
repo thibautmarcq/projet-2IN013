8bbbdc0db4806346963a9c30494859accc5388c3
thibbmrq <thibaut.marcq@gmail.com>
2024-01-26 16:28:56 +0100

    :pencil: Documentation classe Obstacle

--- Code/obstacle.py
+++ Code/obstacle.py
@@ -1,9 +1,12 @@
 class Obstacle :
+    """Obstacle - Objet aux coordonnées discrètes, se place dans la matrice de l'environnement """
 
     def __init__(self, nom, posx, posy) :
+        """Constructeur de l'obstacle - Prend un nom et des coordonnées entières"""
         self.nom = nom
         self.x = posx
         self.y = posy
 
     def presenter_obstacle(self):
+        """Affichage (console) de l'obstacle"""
         return(self.nom + "à la position (" + str(self.x) + ", " + str(self.y) + ")")
