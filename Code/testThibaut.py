30059a4dcc32f3e7aa78cc762b68db7d0239dd0f
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-01-28 22:32:38 +0100

    renommage des attributs de position x et y dans rotationRobot()

--- Code/testThibaut.py
+++ Code/testThibaut.py
@@ -132,7 +132,7 @@ def rotationRobot(angle):
         et refresh le visuel de la direction de notre robot
     """
     robot.rotation(angle)
-    canv.coords(robot_vec, robot.posx, robot.posy, robot.posx+(75*robot.direction[0]), robot.posy+(75*robot.direction[1]))
+    canv.coords(robot_vec, robot.x, robot.y, robot.x+(75*robot.direction[0]), robot.y+(75*robot.direction[1]))
     rotate_robot_rect(canv, robot, angle)
 
 def rotationRobotD(event):
