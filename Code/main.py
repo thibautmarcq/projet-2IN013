480569719b8cfed1f87424a94570b3d78772ec03
Jerome YU <yujerome44@gmail.com>
2024-01-30 15:35:38 +0100

    Modif main.py : Modif des testes

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,8 @@ import environnement as Env
 import obstacle as Obstacle
 import robot as Robot
 
+# Test des méthodes de la classe environnement
+
 # Création de l'envronnement
 env = Env.Environnement(5, 5, 1)
 # Ajout de robot et d'obstacle
@@ -10,21 +12,27 @@ env.addObstacle("obs")
 # En fonction de la taille et en fonction de l'ordre qu'on appel addRobot et addObstacle, ils peuvent se superposer dans la matrice et peuvent se supprimer
 print(env.detect_obs(env.robots[0]))
 
-# Boucle pour savoir où est le robot
-print("--------------Où est le robot ? ¯\_(ツ)_/¯--------------------------")
+# Affichage ASCII pour savoir où sont placés le robot et l'obstacle
+print("--------------Où est le robot et l'obstacle ? ¯\_(ツ)_/¯--------------------------")
+n=0
+print("|", end="")
+print("M", end="")
 for i in range(len(env.matrice)) :
-    for j in range(len(env.matrice)) :
-        print("i :", i, ", j :", j)
-        if ( isinstance(env.matrice[i][j], Robot.Robot) ) :
-            env.matrice[i][j].afficher_etat()
-        else :
-            print("Ce n'est pas un robot")
-# Boucle pour savoir où est l'obstacle
-print("--------------Où est l'obstacle ? ¯\_(ツ)_/¯--------------------------")
+    print("|", end="")
+    print(n, end="")
+    n+=1
+print("|")
+x=0
 for i in range(len(env.matrice)) :
+    print("|", end="")
+    print(x,end="")
     for j in range(len(env.matrice)) :
-        print("i :", i, ", j :", j)
+        print("|", end="")
         if ( isinstance(env.matrice[i][j], Obstacle.Obstacle) ) :
-            print(env.matrice[i][j].presenter_obstacle())
-        else :
-            print("Ce n'est pas un obstacle")
+            print("o", end="")
+        elif ( isinstance(env.matrice[i][j], Robot.Robot) ) :
+            print("r", end="")
+        else:
+            print("x", end="")
+    print("|")
+    x+=1
