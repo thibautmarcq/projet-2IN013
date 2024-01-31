a251f98f46ac8a16412e9546d7dd64d2d376de6c
Jerome YU <yujerome44@gmail.com>
2024-01-31 21:49:38 +0100

    Modif main.py : Petite modification pour tester environnement.py après les changements

--- Code/main.py
+++ Code/main.py
@@ -6,33 +6,16 @@ import robot as Robot
 
 # Création de l'envronnement
 env = Env.Environnement(5, 5, 1)
+
 # Ajout de robot et d'obstacle
 env.addRobot("rob", 0, 0, 1, 1, 1)
 env.addObstacle("obs")
+
 # En fonction de la taille et en fonction de l'ordre qu'on appel addRobot et addObstacle, ils peuvent se superposer dans la matrice et peuvent se supprimer
 print(env.detect_obs(env.robots[0]))
 
-# Affichage ASCII pour savoir où sont placés le robot et l'obstacle
+# Affichage de la matrice
 print("--------------Où est le robot et l'obstacle ? ¯\_(ツ)_/¯--------------------------")
-n=0
-print("|", end="")
-print("M", end="")
-for i in range(len(env.matrice)) :
-    print("|", end="")
-    print(n, end="")
-    n+=1
-print("|")
-x=0
-for i in range(len(env.matrice)) :
-    print("|", end="")
-    print(x,end="")
-    for j in range(len(env.matrice)) :
-        print("|", end="")
-        if ( isinstance(env.matrice[i][j], Obstacle.Obstacle) ) :
-            print("o", end="")
-        elif ( isinstance(env.matrice[i][j], Robot.Robot) ) :
-            print("r", end="")
-        else:
-            print("x", end="")
-    print("|")
-    x+=1
+
+# Le chiffre 1 représente le robot et le chiffre 2 représente l'obstacle
+print(env.matrice)
