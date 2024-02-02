d6316597eece5137cf4648f7ec8117313ffde245
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-02-02 01:36:04 +0100

    Premier commit avec definitions des fonctions

---
+++ Test/test_env.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+
+from Code.environnement import Environnement
+
+class TestEnvironnement(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_constructeur(self):
+        pass
+
+    def test_addRobot(self):
+        pass
+
+    def test_addObstacle(self):
+        pass
+
+    def test_detect_obs(self):
+        pass
+
+    def test_affiche(self):
+        # Methode non définie dans Environnement
+        pass
