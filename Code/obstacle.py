c01d33b9acab0758e93eb19e03fe42e25d89907d
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-02-02 12:47:40 +0100

    docstring du constrcteur de la classe obstacle

--- Code/obstacle.py
+++ Code/obstacle.py
@@ -3,7 +3,13 @@ class Obstacle :
     """
 
     def __init__(self, nom, x, y) :
-        """Constructeur de l'obstacle - Prend un nom et des coordonnées entières"""
+
+        """ Constructeur de l'obstacle, il crée et initialise un obstacle en fonction des coordonnées passées en paramètre
+            :param x: coordonnée x initiale de l'obstacle
+            :param y: coordonnée y initiale de l'obstacle
+            :returns: ne retourne rien, crée uniquement l'obstacle
+        """
+        
         self.nom = nom
         self.x = x
         self.y = y
