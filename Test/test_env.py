8488b02a85404d0286d61bb4d5832ffb3d9e2f3f
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-02-02 20:11:25 +0100

    Mise à jour gitignore + ajout de tests Environnement

--- Test/test_env.py
+++ Test/test_env.py
@@ -1,7 +1,10 @@
+# python3 -m unittest Test/test_env.py -v
+
 import unittest
 import numpy as np
 
 from Code.environnement import Environnement
+from Code.robot import Robot
 
 class TestEnvironnement(unittest.TestCase):
     def setUp(self):
@@ -10,18 +13,23 @@ class TestEnvironnement(unittest.TestCase):
 
     def test_constructeur(self):
         self.assertEqual(self.env.width, 40)
-        self.assertEqual(self.env.height, 40)
+        self.assertEqual(self.env.height, 30)
         self.assertIsInstance(self.env.matrice, np.ndarray)
         self.assertEqual(len(self.env.robots), 0)
         self.assertEqual(self.env.scale, 5)
         
 
-    # def test_addRobot(self):
-    #     pass
+    def test_addRobot(self):
+        self.assertEqual(len(self.env.robots), 0)
+        self.env.addRobot("Bobby", 18, 14.5, 5, 8, 0)
+        self.assertEqual(len(self.env.robots), 1)
+        self.assertIsInstance(self.env.robots[0], Robot)
+        # Ajouter test après condition dehors environnement
 
-    # def test_addObstacle(self):
-    #     pass
-
+    def test_addObstacle(self):
+        self.env.addObstacle("Roger")
+        self.assertTrue(2 in self.env.matrice)
+        
     # def test_detect_obs(self):
     #     pass
 
