36fbf363efdad9e512e0edd9d057b0f6c66f0118
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-02-11 15:44:22 +0100

    Ajout des nouveaux binds pour la gestion de la vitesse

--- simulation.py
+++ simulation.py
@@ -2,4 +2,4 @@ from Code.Interface.newinterface import Interface
 from Code.environnement import Environnement
 
 run = Interface(850, 650, 25)
-run.initRobot("Bob", 150, 45, 30, 55, 0)
+run.initRobot("Bob", 150, 45, 30, 55, 0, "pink")
