455354ce42130c99c87fdd6e82a1b608740e1df6
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-02-13 19:24:45 +0100

    Fautes d'orthographes (sorry)

--- Code/Controleur/controleur.py
+++ Code/Controleur/controleur.py
@@ -2,7 +2,7 @@ from ..robot import Robot
 
 class StrategieAvancer:
     def __init__(self, distance, vitesse, rob):
-        """ Statégie qui fais avancer le robot d'une distance donnée"""
+        """ Statégie qui fait avancer le robot d'une distance donnée"""
         self.distance = distance
         self.vitesse = vitesse
         self.rob = rob 
@@ -12,23 +12,23 @@ class StrategieAvancer:
     def refresh(self): #Changement de vitesse ?
         pass
 
-    def step(self): #Fais augmenter la distance parcouru ?
+    def step(self): #Fait augmenter la distance parcourue ?
         pass
 
-    def stop(self): # Fais arreter le robot quand la distance est inferieure a parcouru ?
+    def stop(self): # Fait arreter le robot quand la distance est inferieure a parcourue ?
         pass
 
 
 class StrategieTourner:
     def __init__(self, rob, angle):
-        """Stategie qui fais tourner le robot d'un angle donné"""
+        """Stategie qui fait tourner le robot d'un angle donné"""
         self.rob = rob
         self.angle = angle
         #argument temporaire
 
-    def tourne(self): #appele la fonction tourner d robot?
+    def tourne(self): #appelle la fonction tourner d robot?
         pass
 
-    def stop(self): #fais arreter le robot quand il a tourner ?
+    def stop(self): #fait arreter le robot quand il a tourné ?
         pass
 
