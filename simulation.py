1b176391c189a561a7bf56b868dc5bc17a8101ed
maelleliu <maelleliu08@gmail.com>
2024-02-18 22:18:30 +0100

    Changement du scale dans l initialisation de env dans simulation

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 from Code.Interface.interface import Interface
 from Code.environnement import Environnement
 
-env = Environnement(750, 550, 25) # Initialisation de l'env
-env.addObstacle('P',[(700,300),(350,300),(350,350), (300,350)])
+env = Environnement(750, 550, 1) # Initialisation de l'env
+env.addObstacle('P',[(300,300),(350,300),(350,350), (300,350)])
 run = Interface(env) # Lancement de l'interface
 run.initRobot("Bob", 250, 250, 30, 55, 20, "lightgreen") # Initialisation du robot dans l'interface
