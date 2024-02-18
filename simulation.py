fd25aacd2838edb8c7c05620ab4f77d29760bebc
maelleliu <maelleliu08@gmail.com>
2024-02-18 21:57:31 +0100

    Ajout d'un attribut dans addObstacle dans env mais j'ai oublier de le faire dans simulation

--- simulation.py
+++ simulation.py
@@ -2,6 +2,6 @@ from Code.Interface.interface import Interface
 from Code.environnement import Environnement
 
 env = Environnement(750, 550, 25) # Initialisation de l'env
-env.addObstacle([(300,300),(350,300),(350,350), (300,350)])
+env.addObstacle('P',[(700,300),(350,300),(350,350), (300,350)])
 run = Interface(env) # Lancement de l'interface
 run.initRobot("Bob", 250, 250, 30, 55, 20, "lightgreen") # Initialisation du robot dans l'interface
