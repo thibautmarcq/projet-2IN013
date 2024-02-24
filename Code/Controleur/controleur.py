b9afb1477186b4c0e0688206de5e5681c15db9e0
ines-benamer <ines.benamerbelkacem@gmail.com>
2024-02-24 18:51:56 +0100

    stratégie tourner

--- Code/Controleur/controleur.py
+++ Code/Controleur/controleur.py
@@ -1,5 +1,5 @@
 from ..robot import Robot
-
+from Code.outil import *
 
 class StrategieAvancer:
     def __init__(self, rob, distance):
@@ -29,17 +29,31 @@ class StrategieTourner:
         self.rob = rob
         self.angle = angle
         self.angle_parcouru = 0
+        self.dir_depart = self.rob.direction
+        self.debut = 0
 
-    def step(self): # Fait augmenter l'angle parcouru + appelle la fonction touner de robot
-        if (self.angle > 0) :
-            self.angle_parcouru += 1
-            self.rob.TourneGauche()
-        if (self.angle < 0) :
-            self.angle_parcouru -= 1
-            self.rob.TourneDroite()
+    def step(self):
 
-    def stop(self): #fait arreter le robot quand l'angle parcouru est supérieur ou égale à l'angle
-        if self.angle_parcouru >= self.angle or self.angle_parcouru <= self.angle:
+        if (self.debut < 1) : # Dans le cas où c'est le premier step, on initialise tout pour se mettre dans les bonnes conditions
+            self.debut = 1
+            self.rob.setVitesse(0)
+
+            # On considère ici une rotation d'un angle alpha dans le sens horaire, c.à.d si positif on tourne vers la droite, sinon vers la gauche
+            # On change les vitesses des deux roues, en leur donnant des vitesses opposées afin de tourner sur place
+            if self.angle > 0 :
+                self.rob.changeVitAngG(0.1)
+                self.rob.changeVitAngD(-0.1)
+
+            elif self.angle < 0 :
+                self.rob.changeVitAngD(0.1)
+                self.rob.changeVitAngG(-0.1)
+
+        else : # Si ce n'est pas le premier step, on met à jour l'angle parcouru par le robot
+            self.angle_parcouru = getAngleFromVect(self.dir_depart, self.rob.direction)
+
+
+    def stop(self): #fait arreter le robot lorsqu'on a parcouru l'angle souhaité
+        if self.angle_parcouru >= self.angle :
             self.rob.setVitesse(0)
             return True
 
