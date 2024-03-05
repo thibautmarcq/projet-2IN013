82d2c4f6d53d9f7518322035c5aebc3c9576add9
Jerome YU <yujerome44@gmail.com>
2024-03-05 15:39:58 +0100

    Modif mockupRobot.py : Ajout de l'adaptateur du robot réel

--- Code/mockupRobot.py
+++ Code/mockupRobot.py
@@ -1,4 +1,5 @@
-from Code.robot import *
+
+
 
 class mockupRobot():
     def stop(self):
@@ -11,6 +12,7 @@ class mockupRobot():
         pass
 
     def set_motor_dps(self, port, dps):
+        print("set_motor_dps", port, dps)
         pass
 
     def get_motor_position(self):
@@ -36,3 +38,47 @@ class mockupRobot():
 
     def __getattr__(self,attr):
         pass
+
+class Adaptateur(mockupRobot) :
+    def __inti(self) :
+        mockupRobot.__init__(self)
+
+    def setVitAngD(self, dps) :
+        self.set_motor_dps(self.MOTOR_RIGHT, dps)
+
+    def setVitAngG(self, dps) :
+        print("setVitAngG", dps)
+        self.set_motor_dps(self.MOTOR_LEFT, dps)
+
+    def setVitAng(self, dps) :
+        self.set_motor_dps(self.MOTOR_RIGHT + self.MOTOR_LEFT, dps)
+
+    def changeVitAngG(self, quant) :
+        vit = self.get_motor_position(self.MOTOR_LEFT)
+        self.set_motor_dps(self.MOTOR_LEFT, vit + quant)
+
+    def changeVitAngD(self, quant) :
+        vit = self.get_motor_position(self.MOTOR_RIGHT)
+        self.set_motor_dps(self.MOTOR_RIGHT, vit + quant)
+
+    def changeVitAng(self, quant) :
+        vitG = self.get_motor_position(self.MOTOR_LEFT)
+        vitD = self.get_motor_position(self.MOTOR_RIGHT)
+        self.changeVitAngG(vitG + quant)
+        self.changeVitAngD(vitD +quant)
+    
+    def getVitesseG(self) :
+        return self.get_motor_position(self.MOTOR_LEFT)
+    
+    def getVitesseD(self) :
+        return self.get_motor_position(self.MOTOR_RIGHT)
+    
+    def getVitesse(self) :
+        vit = self.get_motor_position(self.MOTOR_LEFT) + self.get_motor_position(self.MOTOR_RIGHT)
+        return vit/2
+
+    def capteurDistance(self) :
+        return self.get_distance()
+    
+# mockupRobot = Adaptateur()
+# mockupRobot.setVitAngG(20)
