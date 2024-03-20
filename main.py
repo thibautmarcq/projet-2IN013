c6c835c5e17865ab7596cbb0f3b19d464d163261
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-03-20 19:22:20 +0100

    Ajout statégie arretMur au main du Gopigo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from src.Controleur.controleur import Controler
 from src.Controleur.Strategies import *
-from Robot.gopigo import *
+from src.Robot.gopigo import *
 
 #On crée un controleur
 controleur = Controler()
@@ -10,21 +10,29 @@ controleur = Controler()
 # Ajoute le premier robot
 reel = Adaptateur()
 
-# def menu() :
-#     print("0 - Quitter")
-#     print("1 - Tracer un carré")
-#     print("2 - Avancer vers le mur sans se crasher")
-#     cmd = input("Veuillez choisir une action parmi celles proposées : \n")
-#     match cmd :
-#         case 1:
-#             long = float(input("Quelle largeur voulez-vous pour le carré ? \n"))
-#             carre = setStrategieCarre(reel, long)
-#             controleur.lancerStrategie(carre)
-#         case 2:
-#           pass  
-#         case 3:
-#             pass
-        
-long = float(input("Quelle largeur voulez-vous pour le carré ? \n"))
-carre = setStrategieCarre(reel, long)
-controleur.lancerStrategie(carre)
+def menu() :
+    global RUNNING
+    print("0 - Quitter")
+    print("1 - Tracer un carré")
+    print("2 - Avancer vers le mur sans se crasher")
+    print("3 - Stratégie séquentielle")
+    cmd = int(input("Veuillez choisir une action :\n"))
+    match cmd :
+        case 0:
+            RUNNING = False
+        case 1:
+            long = float(input("Quelle largeur voulez-vous pour le carré ? \n"))
+            carre = setStrategieCarre(reel, long)
+            controleur.lancerStrategie(carre)
+        case 2:
+            long = float(input("A quelle distance doit-on s'arrêter du mur ?"))
+            mur = setStrategieArretMur(reel, long, None)
+            controleur.lancerStrategie(mur)
+        case 3:
+            pass
+        case _:
+            print("Numéro non disponible")
+
+RUNNING = True
+while RUNNING:
+    menu()
