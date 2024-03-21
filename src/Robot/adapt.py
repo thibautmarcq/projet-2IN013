9847be6fc6ae311f90096d4abf1e515a7b22cc84
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-03-21 16:44:30 +0100

    Ajout de la classe abstraite pour les adaptateurs

---
+++ src/Robot/adapt.py
@@ -0,0 +1,23 @@
+class Adaptateur:
+    """ Classe abstraite d'adaptateur pour définir les méthodes des adaptateurs """
+    
+    def _init_(self):
+        pass
+    
+    def setVitAngDA(self, dps):
+        pass
+    
+    def setVitAngGA(self, dps) :
+        pass
+    
+    def setVitAngA(self, dps) :
+        pass
+    
+    def capteurDistanceA(self) :
+        pass
+
+    def distance_parcourue(self) :
+        pass
+    
+    def angle_parcouru(self) :
+        pass
