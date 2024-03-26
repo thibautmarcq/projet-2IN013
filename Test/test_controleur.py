358748adecd25345292fcbe6a053c47ae226df55
Jerome YU <yujerome44@gmail.com>
2024-03-26 15:02:08 +0100

    Modif test_controleur.py : Modification des teste unitaire du controleur

--- Test/test_controleur.py
+++ Test/test_controleur.py
@@ -10,12 +10,9 @@ class TestControleur(unittest.TestCase):
         self.env = Environnement(70, 55, 1)
         self.rob = Adaptateur_simule("Rob", 10, 15, 5, 7, 8, self.env)
         self.controleur = Controler()
+        self.carre = setStrategieCarre(self.rob, 30)
 
-    def test_setStrategieCarre(self):
-        Controler.setStrategieCarre(self.controleur, self.rob, 20)
-        self.assertAlmostEqual(self.rob.x, 10)
-        self.assertAlmostEqual(self.rob.y, 15)
-
-    def test_setStrategieArretMur(self):
-        Controler.setStrategieArretMur(self.controleur, self.rob, 20)
-        self.assertGreaterEqual(20,self.rob.capteurDistance(self.env))
+    def TestlanStrategie(self):
+        self.controleur.lancerStrategie(self.carre)
+        self.assertEqual(self.controleur.strategie, 1)
+        self.assertEqual(self.controleur.strat_en_cour, self.carre)
