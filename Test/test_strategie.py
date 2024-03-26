75bfb0d37908c522f16c26e7f8d467a813a47ff8
Jerome YU <yujerome44@gmail.com>
2024-03-26 19:32:31 +0100

    Modif test_strategie.py : Ajout de test_strategieAvancer

--- Test/test_strategie.py
+++ Test/test_strategie.py
@@ -18,3 +18,23 @@ class TestControleur(unittest.TestCase):
     def test_setStrategieArretMur(self):
         setStrategieArretMur(self.rob, 20)
         self.assertGreaterEqual(20,self.rob.capteurDistance())
+
+    def test_strategieAvancer(self):
+        self.strat = StrategieAvancer(self.rob, 200)
+        self.strat.start()
+        self.rob.x, self.rob.y = (self.rob.x+50*self.rob.direction[0], self.rob.y+50*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.strat.step()
+        self.assertEqual(self.strat.parcouru, 50.0)
+        self.assertEqual(self.strat.stop(), False)
+        self.rob.x, self.rob.y = (self.rob.x-20*self.rob.direction[0], self.rob.y-20*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.strat.step()
+        self.assertEqual(self.strat.parcouru, 70.0)
+        self.assertEqual(self.strat.stop(), False)
+        self.rob.x, self.rob.y = (self.rob.x+0*self.rob.direction[0], self.rob.y-0*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.strat.step()
+        self.assertEqual(self.strat.parcouru, 70.0)
+        self.assertEqual(self.strat.stop(), False)
+        self.rob.x, self.rob.y = (self.rob.x+130*self.rob.direction[0], self.rob.y+130*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.strat.step()
+        self.assertEqual(self.strat.parcouru, 200.0)
+        self.assertEqual(self.strat.stop(), True)
