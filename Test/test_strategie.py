fac2b36db3cd4138fefa434f15fdefdd024cf560
Jerome YU <yujerome44@gmail.com>
2024-03-26 21:24:46 +0100

    Modif test_strategie.py : Ajout du test unitaire de  StrategieArretMur

--- Test/test_strategie.py
+++ Test/test_strategie.py
@@ -7,7 +7,7 @@ from src.Robot.robot import Adaptateur_simule
 
 class TestControleur(unittest.TestCase):
     def setUp(self):
-        self.env = Environnement(70, 55, 1)
+        self.env = Environnement(100, 100, 1)
         self.rob = Adaptateur_simule("Rob", 10, 15, 5, 7, 8, self.env)
 
     def test_setStrategieCarre(self):
@@ -26,15 +26,15 @@ class TestControleur(unittest.TestCase):
         self.strat.step()
         self.assertEqual(self.strat.parcouru, 50.0)
         self.assertEqual(self.strat.stop(), False)
-        self.rob.x, self.rob.y = (self.rob.x-20*self.rob.direction[0], self.rob.y-20*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.rob.x, self.rob.y = (self.rob.x-20*self.rob.direction[0], self.rob.y-20*self.rob.direction[1]) 
         self.strat.step()
         self.assertEqual(self.strat.parcouru, 70.0)
         self.assertEqual(self.strat.stop(), False)
-        self.rob.x, self.rob.y = (self.rob.x+0*self.rob.direction[0], self.rob.y-0*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.rob.x, self.rob.y = (self.rob.x+0*self.rob.direction[0], self.rob.y-0*self.rob.direction[1]) 
         self.strat.step()
         self.assertEqual(self.strat.parcouru, 70.0)
         self.assertEqual(self.strat.stop(), False)
-        self.rob.x, self.rob.y = (self.rob.x+130*self.rob.direction[0], self.rob.y+130*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 50 dans la direction du robot )
+        self.rob.x, self.rob.y = (self.rob.x+130*self.rob.direction[0], self.rob.y+130*self.rob.direction[1]) 
         self.strat.step()
         self.assertEqual(self.strat.parcouru, 200.0)
         self.assertEqual(self.strat.stop(), True)
@@ -62,4 +62,19 @@ class TestControleur(unittest.TestCase):
         self.rob.direction = (0,-1)
         self.strat.step()
         self.assertAlmostEqual(self.strat.angle_parcouru, 90.0)
+        self.assertEqual(self.strat.stop(), True)
+    
+    def test_strategieArretMur(self):
+        self.strat = StrategieArretMur(self.rob, 10)
+        self.rob.x, self.rob.y = self.env.width/2, self.env.length/2
+        self.strat.start()
+        self.assertEqual(self.strat.distrob, 46)
+        self.assertEqual(self.strat.stop(), False)
+        self.rob.x, self.rob.y = (self.rob.x+0*self.rob.direction[0], self.rob.y+10*self.rob.direction[1]) # On place le robot à une position arbitraire ( remplace le déplacement d'une distance de 10 dans la direction du robot )
+        self.strat.step()
+        self.assertEqual(self.strat.distrob, 36)
+        self.assertEqual(self.strat.stop(), False)
+        self.rob.x, self.rob.y = (self.rob.x+0*self.rob.direction[0], self.rob.y+30*self.rob.direction[1])
+        self.strat.step()
+        self.assertEqual(self.strat.distrob, 6)
         self.assertEqual(self.strat.stop(), True)
