ae12bcad73dfc16fd99a607dd01f4a26faf3ca90
Jerome YU <yujerome44@gmail.com>
2024-03-26 15:26:28 +0100

    Modif test_env.py : Ajout d'un test unitaire de la fonction collision et Ajout de robot simulé dans les test

--- Test/test_env.py
+++ Test/test_env.py
@@ -3,7 +3,7 @@
 import unittest
 
 from src.environnement import Environnement
-from src.Robot.robot import Robot
+from src.Robot.robot import Adaptateur_simule, Robot
 
 
 class TestEnvironnement(unittest.TestCase):
@@ -30,7 +30,7 @@ class TestEnvironnement(unittest.TestCase):
     def test_addRobotSelect(self):
         self.env.addRobotSelect(1) # test d'ajout alors que pas de robot
         self.assertEqual(self.env.robotSelect, 0) 
-        rob0 = Robot('rob0', 10, 10, 5, 5, 2)
+        rob0 = Adaptateur_simule('rob0', 10, 10, 5, 5, 2, self.env)
         rob1 = Robot('rob1', 20, 10, 5, 5, 2)
         self.env.setRobot(rob0, 'lightgreen')
         self.env.addRobotSelect(1) # test avec robot
@@ -47,7 +47,23 @@ class TestEnvironnement(unittest.TestCase):
 
 
     def test_setRobot(self):
-        robot = Robot('rob1', 10, 10, 5, 5, 2)
+        robot = Adaptateur_simule('rob1', 10, 10, 5, 5, 2, self.env)
         self.env.setRobot(robot, 'red')
         self.assertEqual(len(self.env.robots), 1)
         self.assertEqual(self.env.robots[0].couleur, 'red')
+        robot2 = Robot('rob2', 20, 20, 5, 5, 2)
+        self.env.setRobot(robot2, 'green')
+        self.assertEqual(len(self.env.robots), 2)
+        self.assertEqual(self.env.robots[1].couleur, 'green')
+
+    def test_collision(self):
+        robot = Adaptateur_simule('rob1', self.env.width/2, self.env.length/2, 5, 5, 2, self.env) # On place le robot au milieu de l'environnement
+        self.env.setRobot(robot, 'red')
+        self.assertEqual(self.env.collision(robot), False)
+        robot.x, robot.y = 3, self.env.length/2 # On place le robot près des murs de l'environnement pour faire une collision contre le mur
+        self.assertEqual(self.env.collision(robot), True)
+        robot2 = Robot('rob2', 10, 90, 5, 5, 2) # On place le robot au milieu de l'environnement
+        self.env.setRobot(robot2, 'green')
+        self.assertEqual(self.env.collision(robot2), False)
+        robot2.x, robot2.y = self.env.width/2, 3 # On place le robot près des murs de l'environnement pour faire une collision contre le mur
+        self.assertEqual(self.env.collision(robot2), True)
