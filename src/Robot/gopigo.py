538b3f7f21e83dcfc851064f784e6280951978ea
Thibaut Marcq <thibaut.marcq@gmail.com>
2024-03-28 15:37:44 +0100

    Suppressions varibales gopigo + gitignore (DS_Store)

--- src/Robot/gopigo.py
+++ src/Robot/gopigo.py
@@ -2,11 +2,6 @@ from robot2IN013 import Robot2IN013 # Import de l'API, supposée être sur le ro
 import math
 import logging
 
-WHEEL_BASE_WIDTH         = 117  # distance (mm) de la roue gauche a la roue droite.
-WHEEL_DIAMETER           = 66.5 #  diametre de la roue (mm)
-WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi # perimetre du cercle de rotation (mm)
-WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi # perimetre de la roue (mm)
-
 class Adaptateur:
     """
     Classe d'adaptation du robot réel qui hérite de la classe mockupRobot
