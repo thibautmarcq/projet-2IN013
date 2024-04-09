6beced518b78e8d449673bb3b4d07f6e04ad4044
Thibaut M <thibaut.marcq@gmail.com>
2024-04-09 03:29:59 +0200

    Correction FOV des caméras, ajout de touches de switch des caméras

--- src/Interface3D/interface3D.py
+++ src/Interface3D/interface3D.py
@@ -8,20 +8,21 @@ from time import sleep
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from panda3d.core import (Geom, GeomNode, GeomTriangles, GeomVertexData,
-                          GeomVertexFormat, GeomVertexWriter,
-                          OmniBoundingVolume, Point3, Texture, load_prc_file)
+						  GeomVertexFormat, GeomVertexWriter,
+						  OmniBoundingVolume, Point3, Texture, load_prc_file)
 from src import DICO_COULEURS, TIC_SIMULATION, Environnement
 
 load_prc_file('src/Interface3D/source/config.prc')
 
 class Interface3D(ShowBase):
+	""" Classe Interface 3D - Ensemble de méthodes définies pour créer et gérer des objets dans une interface en 3D """
 
 	def __init__(self, env):
 		ShowBase.__init__(self)
 		self.env = env
 		self.createAllRobots()
 		self.createEnvironnement()
-		
+
 		# Bind quitter la fenêtre
 		self.accept('escape', exit)
 		# Bind changement de robot
@@ -31,11 +32,11 @@ class Interface3D(ShowBase):
 		self.son = self.loader.loadSfx("src/Interface3D/source/secret.mp3")
 		self.secret = False
 
-		# self.taskMgr.add(self.upCameraTask, "upCameraTask")
-		self.taskMgr.add(self.frontCameraTask, "frontCameraTask")
+		self.taskMgr.add(self.upCameraTask, "upCameraTask")
+		# self.taskMgr.add(self.frontCameraTask, "frontCameraTask")
 		# self.taskMgr.add(self.spinCameraTask, "spinCameraTask")
 		# self.taskMgr.add(self.backCameraTask, "backCameraTask")
-  
+
 
 		self.camLens.setFar(100000) # Pour ne pas avoir de problème de distance de vue
 		T_tictac = Thread(target=self.ticTac, daemon=True)
@@ -56,6 +57,10 @@ class Interface3D(ShowBase):
 		self.accept('x', self.env.addRobotSelect, [1])
 		self.accept('w', self.env.addRobotSelect, [-1])
 		self.accept('shift-m', self.mystere)
+		# Switch entre les vues
+		self.accept('1', self.taskMgr.add, [self.upCameraTask, "upCameraTask"])
+		self.accept('2', self.taskMgr.add, [self.frontCameraTask, "frontCameraTask"])
+		self.accept('3', self.taskMgr.add, [self.backCameraTask, "backCameraTask"])
 
 
 	def createAllRobots(self):
@@ -64,14 +69,14 @@ class Interface3D(ShowBase):
 		"""
 		for robA in self.env.listeRobots:
 			self.createRobot(robA)
-	
+
 	def createRobot(self, robotA):
 		"""Crée un robot en 3D (rectangle) dans l'interface
 		:param robotA: adaptateur de robot
 		:returns: ne retourne rien, ça initalise seulement le robot dans l'interface 3d
 		"""
 		robot = robotA.robot
-		
+
 		# toutes les variables sont reliées à un robot
 		robot.format = GeomVertexFormat.getV3() # type des sommets
 		robot.vdata = GeomVertexData("rectangle", robot.format, Geom.UHDynamic) # UHDynamic car les sommets vont devoir être bougés quand le robot va bouger
@@ -109,7 +114,7 @@ class Interface3D(ShowBase):
 		# Right
 		robot.rectangle.addVertices(1, 3, 7)
 		robot.rectangle.addVertices(7, 5, 1)
-		
+
 		# Créer un objet Geom (=l'objet du robot) pour contenir les triangles (=les faces)
 		robot.geom = Geom(robot.vdata)
 		robot.geom.addPrimitive(robot.rectangle)
@@ -121,13 +126,13 @@ class Interface3D(ShowBase):
 		# Créer un nœud de scène parent pour le nœud de géométrie
 		robot.np = self.render.attachNewNode(robot.node) # np est le noeud de l'objet (=un ptr) dans le moteur graphique
 		robot.np.setPos(0, 0, 0)  # Déplacer le triangle pour le voir
-		
+
 		robot.vectcouleur = DICO_COULEURS[robot.couleur]
 		robot.np.setColor(robot.vectcouleur[0], robot.vectcouleur[1], robot.vectcouleur[2], robot.vectcouleur[3]) # RGB + transparence | COULEUR
 		robot.np.setTwoSided(True) # pour render toutes les faces
 		robot.np.node().setBounds(OmniBoundingVolume())
 		robot.np.node().setFinal(True)
-	
+
 	def updateRobot(self, robotA):
 		""" Update le visuel d'un robot dans l'interface
 		:param robotA: le robotA pour lequel on veut update l'affichage
@@ -135,7 +140,7 @@ class Interface3D(ShowBase):
 		"""
 		robot = robotA.robot
 		dx, dy = robot.direction
-		
+
 		robot.vertex.setRow(0)
 		robot.vertex.setData3f(robot.x-(robot.width/2)*(-dy)-(robot.length/2)*dx, robot.y-(robot.width/2)*(dx)-(robot.length/2)*dy, 0)  # 0 dèrrière bas gauche
 		robot.vertex.setRow(1)
@@ -156,18 +161,18 @@ class Interface3D(ShowBase):
 
 	def createEnvironnement(self):
 		""" Crée le visuel de l'environnement dans l'interface
-		:returns: rien, crée simplement l'environnement en 3D 
+		:returns: rien, crée simplement l'environnement en 3D
 		"""
-		self.env.format = GeomVertexFormat.getV3t2() 
+		self.env.format = GeomVertexFormat.getV3t2()
 		self.env.vdata = GeomVertexData("envi", self.env.format, Geom.UHStatic)
 		self.env.vertex = GeomVertexWriter(self.env.vdata, "vertex")
 		self.env.texcoord = GeomVertexWriter(self.env.vdata, "texcoord")
 
 		# Définition des sommets et des coordonnées de texture
 		self.env.vertex.addData3f(0, 0, -1)  # 0 bas gauche
-		self.env.texcoord.addData2f(0, 0) 
+		self.env.texcoord.addData2f(0, 0)
 		self.env.vertex.addData3f(self.env.width, 0, -1)  # 1 bas droite
-		self.env.texcoord.addData2f(1, 0) 
+		self.env.texcoord.addData2f(1, 0)
 		self.env.vertex.addData3f(0, self.env.length, -1)  # 2 haut gauche
 		self.env.texcoord.addData2f(0, 1)
 		self.env.vertex.addData3f(self.env.width, self.env.length, -1)  # 3 haut droit
@@ -188,31 +193,27 @@ class Interface3D(ShowBase):
 		texture = Texture()
 		texture.read("src/Interface3D/source/envi.png")
 		self.env.np.setTexture(texture)
-		
-	def ticTac(self):
-		while True:
-			# for adapt in self.env.listeRobots:
-			# 	# print('oui')
-			# 	self.updateRobot(adapt)
-			self.binds()
-			sleep(TIC_SIMULATION)
 
-	# Task pour faire rotate la camera
+
+
+	# -------------------- Task pour la camera --------------------
+
 	def spinCameraTask(self, task):
 		""" Effectue une rotation de la caméra autour du robot pendant qu'il avance """
+		self.camLens.setFov(45)
 		robot = self.env.listeRobots[self.env.robotSelect].robot
 		angleDegrees = task.time * 30.0
 		angleRadians = angleDegrees * (pi / 180.0)
 		radius = 300
 		camera_x = robot.x + radius * cos(angleRadians)
 		camera_y = robot.y + radius * sin(angleRadians)
-		self.camera.setPos(camera_x, camera_y, 200) 
+		self.camera.setPos(camera_x, camera_y, 200)
 		self.camera.lookAt(Point3(robot.x, robot.y, 0))
 		return Task.cont
 
-	# Task pour suivre le robot par derrière
 	def backCameraTask(self, task):
 		""" Set la caméra derrière le robot et le suit lorsqu'il avance """
+		self.camLens.setFov(45)
 		robot = self.env.listeRobots[self.env.robotSelect].robot
 		dx, dy = robot.direction
 		camera_x = robot.x + 500 * dx
@@ -221,7 +222,7 @@ class Interface3D(ShowBase):
 		self.camera.setPos(camera_x, camera_y, camera_z)
 		self.camera.lookAt(Point3(robot.x, robot.y, 0))
 		return Task.cont
-	
+
 	def frontCameraTask(self, task):
 		""" Set la caméra devant le robot (comme la vraie caméra) """
 		robot = self.env.listeRobots[self.env.robotSelect].robot
@@ -233,25 +234,27 @@ class Interface3D(ShowBase):
 		self.camera.lookAt(Point3(robot.x + 100 * (-dx), robot.y + 100 * (-dy), robot.height/2))
 		self.camLens.setFov(100) # Réglage du FOV (champ de vision)
 		return Task.cont
-	
+
 	def upCameraTask(self, task):
 		""" Set la caméra au dessus de l'environnement (reproduction interface 2D) """
+		self.camLens.setFov(90)
 		cam_x = (self.env.length/2+100)
 		cam_y = (self.env.width/2-100)
-		cam_z = (self.env.length*2)
+		cam_z = (self.env.length)
 		self.camera.setPos(cam_x, cam_y, cam_z)
 		self.camera.lookAt(Point3(cam_x, cam_y, 0))
 		return Task.cont
-	
+
+
+
 	def ticTac(self):
 		while True:
 			for adapt in self.env.listeRobots:
 				# print('oui')
 				self.updateRobot(adapt)
-				# self.updateCameraTask(adapt)
 			self.binds()
 			sleep(TIC_SIMULATION)
-	
+
 	def mystere(self):
 		""" Méthode secrète """
 		if self.secret==False:
