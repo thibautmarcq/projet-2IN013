42a17b60935d8b332c22b10986b637138e0f0a4a
Thibaut M <thibaut.marcq@gmail.com>
2024-04-24 11:44:50 +0200

    Avancement mercredi 24 youhouuu
    Co-authored-by: Thibaut M <thibaut.marcq@gmail.com>
    Co-authored-by: ines-benamer <ines.benamerbelkacem@gmail.com>
    Co-authored-by: Jerome YU <yujerome44@gmail.com>
    Co-authored-by: maelleliu <maelleliu08@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from .constantes import *
 from .environnement import Environnement
 from .obstacle import Obstacle
 from .outil import *
-from .demoMainAll import menuAll
-
+from .controleur import Controler, StrategieAvancer, StrategieTourner, setStrategieArretMur, setStrategieCarre
+from .robots import Adaptateur_reel, Robot
 # from .demoMainAll import menuAll
+
