40dc09ed75f54be3ab0d939294aeca9642d26758
Jerome YU <yujerome44@gmail.com>
2024-04-27 12:26:03 +0200

    Modif __init__.py  : Ajout de certaines stratégies dans les imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from .constantes import *
+from .controleur import (Controler, StrategieAvancer, StrategieBoucle,
+                         StrategieCond, StrategieSeq, StrategieTourner,
+                         setStrategieArretMur, setStrategieCarre,
+                         verifDistanceSup)
 from .environnement import Environnement
 from .obstacle import Obstacle
 from .outil import *
-from .controleur import Controler, StrategieAvancer, StrategieTourner, setStrategieArretMur, setStrategieCarre
 from .robots import Adaptateur_reel, Robot
-
